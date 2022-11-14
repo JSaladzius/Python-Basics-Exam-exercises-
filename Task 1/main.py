1c869abe6fc92545df34cf0cd287983f4493de21
bulacio <postiklu@gmail.com>
2022-11-14 12:57:10 +0200

    Task 1 - "filterAdultDogOwers" created

--- Task 1/main.py
+++ Task 1/main.py
@@ -5,17 +5,23 @@
 # 2. Grąžins pilnamečius "users", kurie turi augintinį.
 
 users = [
-  { "id": '1', "name": 'John Smith', "age": 20, "hasDog": True },
+  { "id": '1', "name": 'John Smith', "age": 20, "hasDog": True },#
   { "id": '2', "name": 'Ann Smith', "age": 24, "hasDog": False },
-  { "id": '3', "name": 'Tom Jones', "age": 31, "hasDog": True },
+  { "id": '3', "name": 'Tom Jones', "age": 31, "hasDog": True },#
   { "id": '4', "name": 'Rose Peterson', "age": 17, "hasDog": False },
-  { "id": '5', "name": 'Alex John', "age": 25, "hasDog": True },
-  { "id": '6', "name": 'Ronald Jones', "age": 63, "hasDog": True },
+  { "id": '5', "name": 'Alex John', "age": 25, "hasDog": True },#
+  { "id": '6', "name": 'Ronald Jones', "age": 63, "hasDog": True },#
   { "id": '7', "name": 'Elton Smith', "age": 16, "hasDog": True },
   { "id": '8', "name": 'Simon Peterson', "age": 30, "hasDog": False },
-  { "id": '9', "name": 'Daniel Cane', "age": 51, "hasDog": True },
+  { "id": '9', "name": 'Daniel Cane', "age": 51, "hasDog": True },#
 ]
 
 
 def filterAdultDogOwers(users):
-  pass
+  dogowners = []
+  for user in users:
+    if user ["hasDog"] and user["age"] >= 18:
+      dogowners.append(user)
+  return dogowners
+
+print(filterAdultDogOwers(users))
