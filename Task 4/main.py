220f8fe46f57578fdca5a8139f6844c025e93a48
bulacio <postiklu@gmail.com>
2022-11-14 18:49:07 +0200

    Task 4   AddFrame function created

--- Task 4/main.py
+++ Task 4/main.py
@@ -21,5 +21,31 @@
 # Beje, sąraše esantis tekstas, gali būti ir skirtingo ilgio. Todėl rėmelis turėtų būti brėžiamas pagal ilgiausią saraše esantį elementą.
 
 
+
+canvas = [
+  "aaa",
+  "abcddd"  
+]
+
+framed_picture = []
 def addFrame(canvas):
-  pass
+  is_empty_str = list(filter(None, canvas))
+  if not is_empty_str:
+    print("Error: empty canvas provided")
+ 
+  else:
+    longest = max(len(x) for x in is_empty_str)
+    for a in is_empty_str:
+      framed_picture.append("*" * longest + 2 * "*")
+      for a in is_empty_str:
+        short = longest - len(a)
+        framed_picture.append("*"+ a + " " * short + "*")
+      framed_picture.append("*" * longest + 2 * "*")
+      break
+    return framed_picture
+
+addFrame(canvas)
+print(*framed_picture, sep="\n")
+
+
+    
